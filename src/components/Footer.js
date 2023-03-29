9b630e683fb41568fba67a20e5528bd934f1765e
TadeoBernardo13 <tbgb1234@gmail.com>
2023-03-29 03:10:34 -0700

    se agrego vista contacto y servicios.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,102 +1,143 @@
 import React from "react";
 import "../styles/Footer.css";
+import { useState } from "react";
 
 export default function Footer() {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí es donde enviarías el formulario
+    console.log(formValues);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
   return (
     <>
-      <section className="footer d-flex align-items-center row">
-        <div className="container col-lg-4 col-md-6 d-flex align-items-center">
-          <div className="container-fluid">
-            <ul className="list">
-              <li className="list-item">SOLO CON CITA:</li>
-              <li className="list-item">LUNES: 9:00 - 19:00</li>
-              <li className="list-item">MARTES: 9:00 - 19:00</li>
-              <li className="list-item">MIERCOLES: 9:00 - 19:00</li>
-              <li className="list-item">JUEVES: 9:00 - 19:00</li>
-              <li className="list-item">VIERNES: 9:00 - 19:00</li>
-              <li className="list-item">SABADO: 9:00 - 19:00</li>
-              <li className="list-item">DOMINGO: 9:00 - 13:00</li>
-            </ul>
-          </div>
-        </div>
-        <div className="container col-lg-4 col-md-6">
-          <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-            <div>
-              <img src="assets/logoSpa.png" width="500px" alt="" className="img-fluid"/>
-            </div>
-            <div className="social-links">
-              <ul className="d-flex justify-content-center align-items-center">
-                <li>
-                  <a href="https://www.facebook.com/elissanugalbeautycare" target="_blank">
-                    <i class="fa-brands fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com/elissanugalbc/" target="_blank">
-                    <i class="fa-brands fa-instagram"></i>
-                  </a>
-                </li>
+      <section className="footer d-flex flex-column align-items-center" id="contactoForm">
+        <div className="row d-flex align-items-center justify-content-center row-container">
+          <div className="container col-lg-4 col-md-6">
+            <div className="container-fluid">
+              <ul className="list d-flex flex-column align-items-center justify-content-center">
+                <li className="list-item">SOLO CON CITA:</li>
+                <li className="list-item">LUNES: 9:00 - 19:00</li>
+                <li className="list-item">MARTES: 9:00 - 19:00</li>
+                <li className="list-item">MIERCOLES: 9:00 - 19:00</li>
+                <li className="list-item">JUEVES: 9:00 - 19:00</li>
+                <li className="list-item">VIERNES: 9:00 - 19:00</li>
+                <li className="list-item">SABADO: 9:00 - 19:00</li>
+                <li className="list-item">DOMINGO: 9:00 - 13:00</li>
               </ul>
             </div>
           </div>
-        </div>
-        <div className="container col-lg-4 col-md-6">
-          <div className="row justify-content-center mt-4">
-            <div className="col-lg-9">
-              <form
-                action="forms/contact.php"
-                method="post"
-                role="form"
-                className="php-email-form"
-              >
-                <div className="row">
-                  <div className="col-md-6 form-group">
-                    <input
-                      type="text"
-                      name="name"
-                      className="form-control"
-                      id="name"
-                      placeholder="Tu nombre"
-                      required
-                    />
-                  </div>
-                  <div className="col-md-6 form-group mt-3 mt-md-0">
-                    <input
-                      type="email"
-                      className="form-control"
-                      name="email"
-                      id="email"
-                      placeholder="Tu Email"
-                      required
-                    />
-                  </div>
-                </div>
-                <div className="form-group mt-3">
+
+          <div className="container col-lg-4 col-md-6">
+            <div className="container-fluid d-flex flex-column align-items-center">
+              <div>
+                <img
+                  src="assets/logoSpa.png"
+                  width="300px"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div className="social-links d-flex justify-content-center align-items-center">
+                <ul className="d-flex flex-row">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/elissanugalbeautycare"
+                      target="_blank"
+                    >
+                      <i class="fa-brands fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/elissanugalbc/"
+                      target="_blank"
+                    >
+                      <i class="fa-brands fa-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="container col-lg-4 col-md-6 formulario">
+            <div className="container">
+              <h1 className="text-center mb-5">Agenda tu cita</h1>
+              <form onSubmit={handleSubmit} className="row">
+                <div className="col-md-6">
                   <input
+                    placeholder="Tu nombre"
                     type="text"
                     className="form-control"
-                    name="subject"
-                    id="subject"
-                    placeholder="Asunto"
+                    id="name"
+                    name="name"
+                    value={formValues.name}
+                    onChange={handleChange}
                     required
                   />
                 </div>
-                <div className="form-group mt-3">
-                  <textarea
+                <div className="col-md-6 mt-3 mt-md-0">
+                  <input
+                    placeholder="Correo electrónico"
+                    type="email"
                     className="form-control"
-                    name="message"
-                    rows="5"
+                    id="email"
+                    name="email"
+                    value={formValues.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mt-3">
+                  <input
+                    placeholder="Asunto"
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    name="subject"
+                    value={formValues.subject}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mt-3">
+                  <textarea
                     placeholder="Mensaje"
+                    className="form-control"
+                    id="message"
+                    name="message"
+                    rows="3"
+                    value={formValues.message}
+                    onChange={handleChange}
                     required
                   ></textarea>
                 </div>
-                <div className="text-center mt-2">
+                <div className="text-center mt-4">
                   <button type="submit" className="btn btn-outline-dark">
                     Send Message
                   </button>
                 </div>
               </form>
             </div>
+          </div>
+        </div>
+        <div className="row row-container address-row">
+          <div className="container">
+            <h3 className="text-center">
+              +52 686 198 3933 • Ave. Ramos Arizpe #1399, Col. Roma 21250 •
+              Mexicali, Baja California, Mexico
+            </h3>
           </div>
         </div>
       </section>
--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -1,55 +1,157 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import '../styles/Servicios.css'
+import "../styles/Servicios.css";
 
 export default function Servicios() {
   return (
-    <section className="content d-flex justify-content-center align-items-center">
-      <div className="container-fluid">
-        <Fade bottom>
-          <p className="mb-5 titleText">We offer you of our services.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">HAIR EXTENSIONS • 60 MINUTES <strong>$120</strong> / 80 minutes <strong>$145</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">COLOR DESIGN • 60 MINUTES <strong>$115</strong> / 80 minutes <strong>$140</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">COLORÍMETRIA • 60 MINUTES <strong>$85</strong> / 80 minutes <strong>$120</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">MICROBLADING • 60 MINUTES <strong>$100</strong> / 80 minutes <strong>$130</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">ESCULPIMIENTO MUSCULAR • 80 MINUTES <strong>$135</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">DEPILACIÓN LASER • 60 MINUTES <strong>$90</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">MASAJES REDUCTIVOS • 30 MINUTES <strong>$45</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">MAKE UP & HAIR STYLE • 60 MINUTES <strong>$115</strong> / 80 minutes <strong>$140</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">HAIR SHIELD • 60 MINUTES <strong>$100</strong> / 80 minutes <strong>$130</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-        <Fade bottom>
-          <h3 className="Text">BOTOX & FILLERS • 60 MINUTES <strong>$120</strong> / 80 minutes <strong>$145</strong></h3>
-          <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh sit amet ligula eleifend, sit amet vestibulum sem convallis. Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu orci.</p>
-        </Fade>
-      </div>
-    </section>
+    <main>
+      <section className="heroServs">{/* Background Hero in CSS */}</section>
+      <section className="contentServs d-flex justify-content-center align-items-center">
+        <div className="containerCont">
+          <div className="row">
+            <div className="col-lg-8 col-md-12">
+              <Fade bottom>
+                <p className="mb-5 titleText">We offer you of our services.</p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  HAIR EXTENSIONS • 60 MINUTES <strong>$120</strong> / 80
+                  minutes <strong>$145</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  COLOR DESIGN • 60 MINUTES <strong>$115</strong> / 80 minutes{" "}
+                  <strong>$140</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  COLORÍMETRIA • 60 MINUTES <strong>$85</strong> / 80 minutes{" "}
+                  <strong>$120</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  MICROBLADING • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
+                  <strong>$130</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  ESCULPIMIENTO MUSCULAR • 80 MINUTES <strong>$135</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  DEPILACIÓN LASER • 60 MINUTES <strong>$90</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  MASAJES REDUCTIVOS • 30 MINUTES <strong>$45</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  MAKE UP & HAIR STYLE • 60 MINUTES <strong>$115</strong> / 80
+                  minutes <strong>$140</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  HAIR SHIELD • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
+                  <strong>$130</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+              <Fade bottom>
+                <h3 className="Text">
+                  BOTOX & FILLERS • 60 MINUTES <strong>$120</strong> / 80
+                  minutes <strong>$145</strong>
+                </h3>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                  orci.
+                </p>
+              </Fade>
+            </div>
+            <div className="col-lg-4 col-md-12 d-flex flex-column align-items-center contacto">
+              <Fade>
+                <p>For reservations or information:</p>
+                <div className="mt-3 text-center">
+                  <a href="#contactoForm" className="btn btn-outline-info rounded-pill p-4">CONTACTANOS</a>
+                </div>
+              </Fade>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
