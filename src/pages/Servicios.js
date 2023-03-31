5f63bc3e5ecbc883f3c689a1dbf42d018baf650c
TadeoBernardo13 <tbgb1234@gmail.com>
2023-03-31 16:47:18 -0700

    fade removed

--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -9,136 +9,123 @@ export default function Servicios() {
         <div className="containerCont">
           <div className="row">
             <div className="col-lg-8 col-md-12">
-              <Fade bottom>
-                <p className="mb-5 titleText">We offer you of our services.</p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  HAIR EXTENSIONS • 60 MINUTES <strong>$120</strong> / 80
-                  minutes <strong>$145</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  COLOR DESIGN • 60 MINUTES <strong>$115</strong> / 80 minutes{" "}
-                  <strong>$140</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  COLORÍMETRIA • 60 MINUTES <strong>$85</strong> / 80 minutes{" "}
-                  <strong>$120</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  MICROBLADING • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
-                  <strong>$130</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  ESCULPIMIENTO MUSCULAR • 80 MINUTES <strong>$135</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  DEPILACIÓN LASER • 60 MINUTES <strong>$90</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  MASAJES REDUCTIVOS • 30 MINUTES <strong>$45</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  MAKE UP & HAIR STYLE • 60 MINUTES <strong>$115</strong> / 80
-                  minutes <strong>$140</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  HAIR SHIELD • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
-                  <strong>$130</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
-              <Fade bottom>
-                <h3 className="Text">
-                  BOTOX & FILLERS • 60 MINUTES <strong>$120</strong> / 80
-                  minutes <strong>$145</strong>
-                </h3>
-                <p className="mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                  sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
-                  sit amet ligula eleifend, sit amet vestibulum sem convallis.
-                  Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
-                  orci.
-                </p>
-              </Fade>
+              <p className="mb-5 titleText">We offer you of our services.</p>
+              <h3 className="Text">
+                HAIR EXTENSIONS • 60 MINUTES <strong>$120</strong> / 80 minutes{" "}
+                <strong>$145</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                COLOR DESIGN • 60 MINUTES <strong>$115</strong> / 80 minutes{" "}
+                <strong>$140</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                COLORÍMETRIA • 60 MINUTES <strong>$85</strong> / 80 minutes{" "}
+                <strong>$120</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                MICROBLADING • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
+                <strong>$130</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                ESCULPIMIENTO MUSCULAR • 80 MINUTES <strong>$135</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                DEPILACIÓN LASER • 60 MINUTES <strong>$90</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                MASAJES REDUCTIVOS • 30 MINUTES <strong>$45</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                MAKE UP & HAIR STYLE • 60 MINUTES <strong>$115</strong> / 80
+                minutes <strong>$140</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                HAIR SHIELD • 60 MINUTES <strong>$100</strong> / 80 minutes{" "}
+                <strong>$130</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
+
+              <h3 className="Text">
+                BOTOX & FILLERS • 60 MINUTES <strong>$120</strong> / 80 minutes{" "}
+                <strong>$145</strong>
+              </h3>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                sagittis sit amet sapien non hendrerit. Sed sollicitudin nibh
+                sit amet ligula eleifend, sit amet vestibulum sem convallis.
+                Fusce arcu tellus, venenatis vitae tincidunt eget, congue eu
+                orci.
+              </p>
             </div>
             <div className="col-lg-4 col-md-12 d-flex flex-column align-items-center contacto">
               <p>For reservations or information:</p>
